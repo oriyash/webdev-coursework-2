6c37fc7faf797ed67f041b4a42a85ef4825aff3e
Yash Ori <yash.ori@eisti.fr>
2020-12-01 12:17:59 +0000

    first try at login implementation

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ const PORT = 3000
 // set a static folder 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.post('/login.html', (req, res) => {
+    res.redirect('/home')
+})
+
+app.get('/home', (req, res) => {
+    res.send('Login Worked')
+})
+
 app.listen(PORT, () => console.log('Server started on port ' + PORT))
