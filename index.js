6e562e8e85cc0bb08f675511d6986dcc5ba85851
Yash Ori <yash.ori@eisti.fr>
2020-12-03 15:16:21 +0000

    added post req routing

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const PORT = 3000
 // set a static folder 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/login.html', (req, res) => {
-    res.redirect('/home')
+// POST req for login
+app.post('/home', (req, res) => {
+    res.send('Login Worked')
 })
 
-app.get('/home', (req, res) => {
-    res.send('Login Worked')
+// POST req for sign up 
+app.post('/register', (req, res) => {
+    res.redirect('/login.html')
 })
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT))
