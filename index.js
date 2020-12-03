cfe8c04f66081c1b6bf71e2cdf213e2e807bdaef
Pattrawut Thanomsing <p.thanomsing@outlook.com>
2020-12-03 14:37:51 +0000

    static route + 404 page

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path from 'path'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const PORT = 3000
 
 // set a static folder 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT))
