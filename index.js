9e22f2e9427175ae86abbe981ae6aae5d06ac81f
Yash Ori <yash.ori@eisti.fr>
2020-12-09 13:15:34 +0000

    mockup for regiser page

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.post('/register', (req, res) => {
 })
 
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT))
+app.listen(PORT, () => {console.log('Server started on port ' + PORT)})
